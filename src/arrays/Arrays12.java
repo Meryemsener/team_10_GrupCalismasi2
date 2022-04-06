11c57f2d6662ace062f329630ce8fb91cbf71a16
berkc <b.cal.2193@gmail.com>
2022-04-07 00:04:24 +0200

    Arrays12_Ok

--- src/arrays/Arrays12.java
+++ src/arrays/Arrays12.java
@@ -1,5 +1,8 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Arrays12 {
      /*
     N elemanli bir tamsayi dizisi veriliyor, diziyi sondan basa dogru ters cevirin
@@ -8,4 +11,31 @@ public class Arrays12 {
     =>ikinci yolu da temp kullanarak cozun
     ipucu=>bir temp olusturup son elemani ile indekse atma ve bunu length bitene kadar devam etme
      */
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Eleman sayisi giriniz : ");
+
+
+        int n = scan.nextInt();             // n ---> arrayin kac elemani olacagini belirler.
+        int[] arr = new int[n];             // n elemanli bir array olusturuldu.
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + 1 + ". elemani giriniz : ");
+            int eleman = scan.nextInt();    // array´e teker teker eleman atandi.
+            arr[i] = eleman;
+        }
+        System.out.println("Arr : " + Arrays.toString(arr));   // arr yazdirildi.
+
+
+        int temp[] = new int[arr.length];   // arr uzunlugunda yeni bir array olusturuldu --> reverse icin
+
+        for (int i = temp.length - 1; i >= 0; i--) {
+            temp[i] = arr[temp.length - i - 1]; // temp array´ine arr reverse edilerek atandi.
+        }
+        System.out.println(Arrays.toString(temp));
+    }
 }
