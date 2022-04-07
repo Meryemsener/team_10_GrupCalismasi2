af03f4911d765595bc32f313548c5a1bf64bd73f
m.akif.oren <makif.oeren@gmail.com>
2022-04-07 10:42:28 +0200

    ödev teslimi-1

--- src/arrays/Arrays01.java
+++ src/arrays/Arrays01.java
@@ -6,4 +6,24 @@ public class Arrays01 {
         Bu notlarin toplamini ve ortalamasini bulunuz
         input={30,56,56,75,95,100,30,80,50}
          */
+    public static void main(String[] args) {
+
+
+        int sayi [] = {30, 56, 56, 75, 95, 100, 30, 80, 50};
+
+        int sum=0;
+
+        for (int i = 0; i < sayi.length; i++) {
+
+            sum=sum + sayi[i];
+        }
+        double average=sum/sayi.length;
+
+
+        System.out.println("sum = " + sum);
+        System.out.println("averge = " + average);
+
+
+    }
+
 }
