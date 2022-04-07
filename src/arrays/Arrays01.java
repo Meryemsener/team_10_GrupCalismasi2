386a5f5b6c37d684268edd782b83603f13b360eb
m.akif.oren <makif.oeren@gmail.com>
2022-04-07 11:34:01 +0200

    son deneme

--- src/arrays/Arrays01.java
+++ src/arrays/Arrays01.java
@@ -8,5 +8,19 @@ public class Arrays01 {
          */
     public static void main(String[] args) {
 
+        int sayi[] = {30, 56, 56, 75, 95, 100, 30, 80, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < sayi.length; i++) {
+
+            sum = sum + sayi[i];
+        }
+        double average = sum / sayi.length;
+
+
+        System.out.println("sum = " + sum);
+        System.out.println("averge = " + average);
     }
+
 }
