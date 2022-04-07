a766979236eba6f8bf43b07e36f9931be486c59f
Ustun Ertas <ustunertas0@gmail.com>
2022-04-07 23:27:29 +0200

    4/7/22

--- src/arrays/Arrays11.java
+++ src/arrays/Arrays11.java
@@ -8,4 +8,13 @@ public class Arrays11 {
         ogrenci 1. siradadir, Ali'nin boyu verilen dizide de yer almaktadir
 
          */
+      //Once Ali'den kac kisi uzun oldugunu bulmak lazim
+      //Sonra Ali ile ayni boyda kac kisi var onu bulmam lazim
+      //Ve son olarakta Ali'nin muhtemel siralari
+
+    public static void main(String[] args) {
+
+        int []boylar = new int []{190,175,185,175,177,178,198,175,187,182 };
+
+    }
 }
