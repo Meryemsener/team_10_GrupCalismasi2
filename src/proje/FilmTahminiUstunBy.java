4854b036dc6a3d588adbf1263c3cc7ed8b82d4a2
atilayduran <atilayduran@gmail.com>
2022-04-07 08:35:11 +0200

    odev teslimi

--- src/proje/FilmTahminiUstunBy.java
+++ src/proje/FilmTahminiUstunBy.java
@@ -20,70 +20,68 @@ import java.util.Scanner;
       kullanıcının her tahmininde doğru ve yanlış tahmin  sayısını console yazdırınız.
       kullanıcının kaybedip veya kazandığını ve tahmin etmesi gereken filmin ismini  console yazdırınız.
      */
-<<<<<<< HEAD
+
      //Tahmin Edilecek Fimler:==>,MUCIZE,UMUT,KARAKOMIK,NEFES,OTEKI,AYLA,ESKIYA,MUTLULUK,KELEBEKLER
 
     public static void main(String[] args) throws IOException {
-=======
-       // Tahmin Edilecek Fimler:==>,MUCIZE,UMUT,KARAKOMIK,NEFES,OTEKI,AYLA,ESKIYA,MUTLULUK,KELEBEKLER
 
-       public static void main(String[] args) throws IOException {
->>>>>>> ustun
-
-        Scanner scan = new Scanner(System.in);//kullanicidan deger almak icin
-
-        System.out.print("*****FILM TAHMINI OYUNUNA HOSGELDINIZ******");
-
-        System.out.println("\nFilm tahimi icin bir harf yaziniz : ");
-
-        String kelime= scan.nextLine();// degisken isminde kelime ekledim scanner referansindan
-
-        //Dongu olusturam surekli dongumuz olmasi icin int deger atiyacam
-        int i, tahmin =0, anahtar=0, dogru=0;// int turunden tanim yaptim hespini bir satirda yaptim
-
-        //Dizi olusturacam String turunden
-        String bulunan[]=new String[kelime.length()];//kelime indexini bulmak icin parametre icine yazdim
-
-        System.out.println("Kelimeyi bulmak icin 5 yanlis hakkiniz var");//kullanicin kac hakkini oldugunu gosterdim
-
-        //googleden arastirdim BufferedReader klavye turunden bir degisken yani klavyden okunanlari icin
-        BufferedReader klavye =new BufferedReader(new InputStreamReader(System.in));//kutuphanedeki siniflar gibi dusundum
+        // Tahmin Edilecek Fimler:==>,MUCIZE,UMUT,KARAKOMIK,NEFES,OTEKI,AYLA,ESKIYA,MUTLULUK,KELEBEKLER
 
 
-        //For dongumu aciyorum asagida
-
-        for (i =0; i<kelime.length(); i++){// kelimenin uzunlugunu bulmak icin bide i++ artitani kullandim
-            bulunan [i]="_ ";// i sirasiyla artiracgi icin buda "_ " kelimemin uzunluguna kadar alt tire girecek
-
-        }
-
-        //Bir while olusturacam
-
-        while (tahmin<=5){ //tahmin sayisini bulmak icin
-
-            System.out.println("Bir harf giriniz (Kalan hakkiniz " + (5- tahmin)+ ") :");
-            tahmin++;
-            String harf= klavye.readLine();//yazilan harfleri daha kolay getirmek icin
-
-            for ( i= 0; i <kelime.length(); i++) {//olur veya olmaz dongusune soktum
-
-                if (kelime.charAt(i)==harf.charAt(0)){// burda kelimenin uyup uymadigini bakiyorum harf olarak
-                    bulunan[i]= harf + " ";// dizi oldugu icin ayni zaman degiskendir
 
 
-<<<<<<< HEAD
-=======
-                    //Devami gelecek....
->>>>>>> ustun
-                }
+            Scanner scan = new Scanner(System.in);//kullanicidan deger almak icin
+
+            System.out.print("*****FILM TAHMINI OYUNUNA HOSGELDINIZ******");
+
+            System.out.println("\nFilm tahimi icin bir harf yaziniz : ");
+
+            String kelime = scan.nextLine();// degisken isminde kelime ekledim scanner referansindan
+
+            //Dongu olusturam surekli dongumuz olmasi icin int deger atiyacam
+            int i, tahmin = 0, anahtar = 0, dogru = 0;// int turunden tanim yaptim hespini bir satirda yaptim
+
+            //Dizi olusturacam String turunden
+            String bulunan[] = new String[kelime.length()];//kelime indexini bulmak icin parametre icine yazdim
+
+            System.out.println("Kelimeyi bulmak icin 5 yanlis hakkiniz var");//kullanicin kac hakkini oldugunu gosterdim
+
+            //googleden arastirdim BufferedReader klavye turunden bir degisken yani klavyden okunanlari icin
+            BufferedReader klavye = new BufferedReader(new InputStreamReader(System.in));//kutuphanedeki siniflar gibi dusundum
+
+
+            //For dongumu aciyorum asagida
+
+            for (i = 0; i < kelime.length(); i++) {// kelimenin uzunlugunu bulmak icin bide i++ artitani kullandim
+                bulunan[i] = "_ ";// i sirasiyla artiracgi icin buda "_ " kelimemin uzunluguna kadar alt tire girecek
 
             }
 
+            //Bir while olusturacam
+
+            while (tahmin <= 5) { //tahmin sayisini bulmak icin
+
+                System.out.println("Bir harf giriniz (Kalan hakkiniz " + (5 - tahmin) + ") :");
+                tahmin++;
+                String harf = klavye.readLine();//yazilan harfleri daha kolay getirmek icin
+
+                for (i = 0; i < kelime.length(); i++) {//olur veya olmaz dongusune soktum
+
+                    if (kelime.charAt(i) == harf.charAt(0)) {// burda kelimenin uyup uymadigini bakiyorum harf olarak
+                        bulunan[i] = harf + " ";// dizi oldugu icin ayni zaman degiskendir
+
+
+                        //Devami gelecek....
+
+                    }
+
+                }
+
+
+            }
 
         }
-
     }
-   }
 
 
 
