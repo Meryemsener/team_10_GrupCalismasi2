76a0a5cb22a760b527706f189396fb084c7715e8
Ustun Ertas <ustunertas0@gmail.com>
2022-04-09 01:01:33 +0200

    son derleme

--- src/arrays/Arrays11.java
+++ src/arrays/Arrays11.java
@@ -8,13 +8,20 @@ public class Arrays11 {
         ogrenci 1. siradadir, Ali'nin boyu verilen dizide de yer almaktadir
 
          */
-      //Once Ali'den kac kisi uzun oldugunu bulmak lazim
+
+      //Once Ali'den kac kisi uzun oldugunu bulmam lazim
       //Sonra Ali ile ayni boyda kac kisi var onu bulmam lazim
       //Ve son olarakta Ali'nin muhtemel siralari
 
     public static void main(String[] args) {
 
-        int []boylar = new int []{190,175,185,175,177,178,198,175,187,182 };
+        int []boylar = new int []{190,170,185,170,177,178,198,170,187,182 };
+
+
+
+
+
+
 
     }
 }
--- src/proje/FilmTahminiUstunBy.java
+++ src/proje/FilmTahminiUstunBy.java
@@ -1,9 +1,5 @@
 package proje;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.*;
 
 public class FilmTahminiUstunBy {
 
@@ -20,103 +16,62 @@ public class FilmTahminiUstunBy {
       kullanıcının her tahmininde doğru ve yanlış tahmin  sayısını console yazdırınız.
       kullanıcının kaybedip veya kazandığını ve tahmin etmesi gereken filmin ismini  console yazdırınız.
      */
-    // Tahmin Edilecek Fimler:==>,MUCIZE,UMUT,KARAKOMIK,NEFES,OTEKI,AYLA,ESKIYA,MUTLULUK,KELEBEKLER
-     private static boolean devam=false;
-    public static void main(String[] args) throws IOException {
 
+    static List<String> film = new ArrayList<>(Arrays.asList("KELEBEK", "MUCIZE", "AYLA", "DUNKIRK" ,
+            "MEMENTO", "GORA", "ESKIYA", "FIGHTCLUB","KARAKOMIK","KILLBILL","AMELIE","XMEN","OLDBOY",
+            "BATMAN","KELOGLAN","TOYSTORY","MUTLULUK","SEVEN"));
 
-        while (true){
-        boolean devam=false;
-
-        Scanner scan = new Scanner(System.in);//kullanicidan deger almak icin
-
-        System.out.print("*****FILM TAHMINI OYUNUNA HOSGELDINIZ******");
-
-        System.out.println("\nFilm tahimi icin bir harf yaziniz : ");
-
-        String kelime= scan.nextLine();// degisken isminde kelime ekledim scanner referansindan
-
-        //Dongu olusturam surekli dongumuz olmasi icin int deger atiyacam
-        int i, tahmin =0, anahtar=0, dogru=0;// int turunden tanim yaptim hespini bir satirda yaptim
-
-        //Dizi olusturacam String turunden
-        String bulunan[]=new String[kelime.length()];//kelime indexini bulmak icin parametre icine yazdim
-
-        System.out.println("Kelimeyi bulmak icin 5 yanlis hakkiniz var");//kullanicin kac hakkini oldugunu gosterdim
-
-        //googleden arastirdim BufferedReader klavye turunden bir degisken yani klavyden okunanlari icin
-        BufferedReader klavye =new BufferedReader(new InputStreamReader(System.in));//kutuphanedeki siniflar gibi dusundum
-
-
-        //For dongumu aciyorum asagida
-
-        for (i =0; i<kelime.length(); i++){// kelimenin uzunlugunu bulmak icin bide i++ artitani kullandim
-            bulunan [i]="_ ";// i sirasiyla artiracgi icin buda "_ " kelimemin uzunluguna kadar alt tire girecek
-
-        }
-
-        //Bir while olusturacam
-
-        while (tahmin<=5){ //tahmin sayisini bulmak icin
-
-            System.out.println("Bir harf giriniz (Kalan hakkiniz " + (5- tahmin)+ ") :");
-            tahmin++;
-            String harf= klavye.readLine();//yazilan harfleri daha kolay getirmek icin
-
-            for ( i= 0; i <kelime.length(); i++) {//olur veya olmaz dongusune soktum
-
-                if (kelime.charAt(i)==harf.charAt(0)){// burda kelimenin uyup uymadigini bakiyorum harf olarak
-                    bulunan[i]= harf + " ";// dizi oldugu icin ayni zaman degiskendir
-
-                    anahtar =1;//true veya false dondurmesi icin anahtarimiz artik sifir degil bir
-                    dogru++;// dogruyu artirdim
-
-                    if (dogru==kelime.length()){// artik dogru ise bildin programdan cik diye yaptim
-
-                        System.out.println("kelime \n" +kelime+ "*********************\n");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("*********FILM TAHMIN OYUNUNA HOSGELDINIZ*********");
+            System.out.print("1-" + (film.size()) + " arasinda bir sayi giriniz(Cikmak icin sayi disinda bir karakter giriniz):");
+            Collections.shuffle(film);
+            int secim = scan.nextInt();
+            while (secim < 1 || secim > film.size()) {
+                System.out.println("Hatali giris yaptiniz");
+                secim = scan.nextInt();
+            }
+            secim--;
+            String secilenfilm = film.get(secim);
+            String gizlifilm = secilenfilm.replaceAll("\\w", "_");
+            int haksayisi = secilenfilm.length() * 2;
+            String[] arrfilm = secilenfilm.split("");
+            String[] gizlifilmarr = gizlifilm.split("");
+            System.out.println("Sectiginiz filmin harf sayisi: " + arrfilm.length);
+            String tahmin = scan.nextLine().replaceAll("\\W", "").toUpperCase();
+            while (haksayisi >= 0) {
+                if (tahmin.equals(secilenfilm) || !Arrays.toString(gizlifilmarr).contains("_")) {
+                    System.out.println("Tebrikler, filmi dogru tahmin ettiniz :) \nFilmin adi: " + secilenfilm);
+                    break;
+                } else if (haksayisi == 0) {
+                    System.out.println("Filmi tahmin edemediniz...\nFilmin adi: " + secilenfilm);
+                    break;
+                } else {
+                    System.out.println("Kalan tahmin sayiniz: " + haksayisi + "\n" + Arrays.toString(gizlifilmarr)
+                            + "\nLütfen filmin adini tahmin ediniz ya da bir adet harf giriniz:");
+                    tahmin = scan.nextLine().replaceAll("\\W", "").toUpperCase();
+                    if (tahmin.length() != 1) haksayisi--;
+                    else if (!secilenfilm.contains(tahmin)) haksayisi--;
+                    else {
+                        for (int i = 0; i < arrfilm.length; i++) {
+                            if (!arrfilm[i].equals(tahmin)) {
+                                arrfilm[i] = "_";
+                            }
+                        }
+                        for (int i = 0; i < arrfilm.length; i++) {
+                            if (!arrfilm[i].equals("_")) {
+                                gizlifilmarr[i] = arrfilm[i];
+                            }
+                        }
+                        haksayisi--;
+                        arrfilm = secilenfilm.split("");
                     }
                 }
-
             }
-
-           if (anahtar==1){
-               anahtar=0;
-               tahmin--;//tahmimizin dusmemesi icin
-
-           }
-            for (i = 0; 1<kelime.length(); i++) {
-                System.out.print(bulunan[i]);
-
-            }
-            System.out.println();
-        } if (dogru != kelime.length()){
-            System.out.println("Uzgunum.... dogru yanit"+kelime);
-
         }
-
-        System.out.println("***********DEVAM ETMEK ISTERMISINIZ?    ('EVET')/' 'HAYIR' ");
-
-        String cevap= scan.nextLine();
-
-        if (cevap.equals("EVET")){
-            devam=true;
-
-        } else if (cevap=="HAYIR"){
-            System.out.println("Tekrar Bekleriz");
-            devam=false;
-
-            break;//programdan cikmak icin
-        }else {
-
-            System.out.println("Tanimsiz kelime giris yaptiniz!");
-            break;
-        }
-
     }
 }
-}
-
-
 
 
 
