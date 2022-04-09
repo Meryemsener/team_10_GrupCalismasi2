91df1529b5422cc4a4386371ba5fc6be859b72eb
Meryem Aydin <meryemaydinsener@gmail.com>
2022-04-09 23:23:55 +0200

    yeni sorular

---
+++ src/arrays/Arrays16.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Arrays16 {
+    /*
+    Bir spor musabakasinda sira ile sporcular parkuru tamamlamaktadir.
+    Her bir sporcu kendisinden onceki tum sporculardan daha hizli bir rekor kirmis sayilir.
+    Bu musabakada toplam kac kez rekor kirilmistir.
+    input:scores dizisi
+    output:rekor sayisi
+     */
+}
---
+++ src/arrays/Arrays17.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class Arrays17 {
+    public static void main(String[] args) {
+
+    }
+}
