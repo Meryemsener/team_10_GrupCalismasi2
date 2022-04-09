601dbdde6765e2829e9a6eee0a9ee2a57920a956
Meryem Aydin <meryemaydinsener@gmail.com>
2022-04-09 23:58:34 +0200

    yeni sorular

---
+++ src/yeniSorular/Array4.java
@@ -0,0 +1,12 @@
+package yeniSorular;
+
+public class Array4 {
+    /*
+        Bir alışveriş listesi hazırlıyoruz.
+        Ve listede bir elemanı yanlış yazdığımızı fark ettik,
+         düzeltip yerine başka bir eleman eklemek istiyoruz.
+         Yani dizide bir elemanı silip yerine başka bir eleman
+         ekleyeceğiz.
+        Peki bunu Java’da nasıl oluşturacağız ?
+         */
+}
