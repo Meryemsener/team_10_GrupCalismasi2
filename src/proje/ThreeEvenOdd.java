c2bb8e563a5171893f064d1cf14732116f539b2c
m.akif.oren <makif.oeren@gmail.com>
2022-04-10 00:27:10 +0200

    v2

--- src/proje/ThreeEvenOdd.java
+++ src/proje/ThreeEvenOdd.java
@@ -25,28 +25,32 @@ public class ThreeEvenOdd {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Eleman sayisini giriniz");
-        int elemanSayisi=scan.nextInt();
-        int arr[]=new int[elemanSayisi];
+        System.out.println("Eleman sayisini giriniz : ");
+        int elemanSayisi = scan.nextInt();
 
+        int arr[] = new int[elemanSayisi];
 
-        int sayacCift=0;
-        int sayacTek=0;
+        for (int i = 0; i < elemanSayisi; i++) {
+            System.out.println(i + 1 + ". elemani giriniz");
+            int elemanlar = scan.nextInt();
+        }
 
+        int sayacCift = 0;
+        int sayacTek = 0;
+        boolean ücTaneMi = true;
 
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]%2==0){
+            if (arr[i] % 2 == 0) {
                 sayacCift++;
-
-            }else
-                sayacTek++;
+            } else sayacTek++;
         }
-        System.out.println("Cift sayi adedi : " + sayacCift );
+        System.out.println("Cift sayi adedi : " + sayacCift);
         System.out.println("Tek sayi adedi : " + sayacTek);
 
-
-
+        if (sayacCift >= 3 || sayacTek >= 3) {
+            System.out.println(ücTaneMi);
+        } else System.out.println(!ücTaneMi);
 
 
     }
-}
+    }
