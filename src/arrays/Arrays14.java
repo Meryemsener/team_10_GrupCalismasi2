c326b46871fae7ca9a045e04d5cde9e3cd1d14bc
Meryem Aydin <meryemaydinsener@gmail.com>
2022-04-09 12:42:26 +0200

    cozum

--- src/arrays/Arrays14.java
+++ src/arrays/Arrays14.java
@@ -8,5 +8,23 @@ public class Arrays14 {
         input={100,70,80,70,20,30,50,50,65,55,57}
         output
         ortalamayai bulan ve asan ogrenci sayisi:
-   */
+
+     */
+    public static void main(String[] args) {
+        int[] puan = new int[]{100, 70, 80, 70, 20, 30, 50, 50, 65, 55, 57};
+        int toplam = 0;
+        double avarage;
+        int basariliSayisi = 0;
+        for (int i = 0; i < puan.length; i++) {
+            toplam = toplam + puan[i];
+        }
+        avarage = toplam * 1.0 / puan.length;//kesirli sayi olarak almak icin 1.0 ile carp
+        for (int i = 0; i < puan.length; i++) {
+            if (puan[i] >= avarage) basariliSayisi++;
+
+        }
+        System.out.println(avarage);
+        System.out.println("ortalamayai bulan ve asan ogrenci sayisi : " + basariliSayisi);
+
+    }
 }
