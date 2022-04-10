10c68e53fb034d2fa3b3d9d75ab8597c82b280d9
atilayduran <atilayduran@gmail.com>
2022-04-10 12:36:33 +0200

    odev teslimi2

--- src/arrays/Arrays01.java
+++ src/arrays/Arrays01.java
@@ -8,10 +8,7 @@ public class Arrays01 {
          */
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-=======
 
->>>>>>> aakiff
         int sayi[] = {30, 56, 56, 75, 95, 100, 30, 80, 50};
 
         int sum = 0;
@@ -25,10 +22,7 @@ public class Arrays01 {
 
         System.out.println("sum = " + sum);
         System.out.println("averge = " + average);
-<<<<<<< HEAD
-=======
 
->>>>>>> aakiff
     }
 
 }
