1461c956492be31aad000d8a91e020c5f6969513
KYkadir <101402050+KYkadir@users.noreply.github.com>
2022-04-14 19:32:28 +0200

    arraylist

--- src/yeniSorular/ArrayList.java
+++ src/yeniSorular/ArrayList.java
@@ -1,5 +1,8 @@
 package yeniSorular;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class ArrayList<S> {
     public static void main(String[] args) {
         /*
@@ -10,5 +13,36 @@ public class ArrayList<S> {
           Yani dizideki herhangi bir elemanın sırasını yazdıran programı
            “array” ile yazacagiz.
          */
-    }
-}
+
+
+
+                Scanner scan=new Scanner(System.in);
+                System.out.println("Verilecek Olan Eğitimde Kaç Adet Ders Almak İstiyorsunuz  ?");
+                int dersSayisi=scan.nextInt();
+                List<String> dersler=new java.util.ArrayList<>();
+                System.out.println("Almak İstediğiniz Dersleri Yazınız...");
+
+                for(int i=0;i<dersSayisi;i++){
+                    dersler.add(scan.next());
+                }
+                System.out.println("Aldığınız Dersler :");
+                for(String ders :dersler){
+                    System.out.println(ders);
+                }
+                System.out.println("Eklediğiniz Derslerden Hangisinin Sırasını Öğrenmek İstiyorsunuz?");
+                String ara=scan.next();
+                System.out.println();
+                if(dersler.contains(ara)){
+
+                    System.out.println("Eklediğiniz Ders :"+ ara);
+                    System.out.println("Eklediğiniz Dersin Eğitimini "+(dersler.indexOf(ara)+1)+". Sırada Alacaksınız");
+
+                }else{
+                    System.out.println("Aradığınız Ders, Seçimlerinizde Gözükmemektedir.");
+
+                }
+            }
+        }
+
+
+
