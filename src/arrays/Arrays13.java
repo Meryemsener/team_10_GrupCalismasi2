6b3bc8befef87709e9b11ef525defca16e2660d3
Mulkiye Ayboy <mlkazby650@gmail.com>
2022-04-15 23:03:19 +0200

    "cozum"

--- src/arrays/Arrays13.java
+++ src/arrays/Arrays13.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class Arrays13 {
     /*
             ilk iki elemani sira ile 0 ve 1 olan, N elemanli bir tamsayi dizisinin
@@ -13,4 +15,21 @@ public class Arrays13 {
              numbers={0,1,1,2,3,5,8,13}
 
             */
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int N= scanner.nextInt();
+        int sayilar[]=new int[N];
+         sayilar[0]=0;
+         sayilar[1]=1;
+
+        for (int i = 2; i <sayilar.length ; i++) {
+            sayilar[i]=sayilar[i-1]+sayilar[i-2];
+        }
+
+        for (int i = 0; i <sayilar.length ; i++) {
+            System.out.print(sayilar[i]+" ");
+
+        }
+    }
 }
